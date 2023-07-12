a7afcee0a0e361eb2f7fba2cccdb4d1bb510f654
Chocolate covered Strawberry <75491899+lhpark114@users.noreply.github.com>
2023-07-12 17:55:40 -0400

    JS - active_menu_options

--- src/active_menu.js
+++ src/active_menu.js
@@ -5,7 +5,10 @@ const navItems = sectionIds.map((id) => document.querySelector(`[href="${id}"]`)
 
 const visibleSections = sectionIds.map(() => false);
 
-const options = {};
+const options = {
+    rootMargin: '-20% 0px 0px 0px',
+    threshold: [0, 0.98],
+};
 const observer = new IntersectionObserver(observerCallback, options);
 sections.forEach(section => observer.observe(section));
 
