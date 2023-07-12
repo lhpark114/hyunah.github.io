a8f0c125211c524bddc7b07e156c99d56fe573b9
Chocolate covered Strawberry <75491899+lhpark114@users.noreply.github.com>
2023-07-12 17:44:03 -0400

    JS-active_menu(2)

--- src/active_menu.js
+++ src/active_menu.js
@@ -2,14 +2,33 @@ const sectionIds = ['#home', '#about', '#skills', '#work', '#testimonials', '#co
 const sections = sectionIds.map((id) => document.querySelector(id));
 const navItems = sectionIds.map((id) => document.querySelector(`[href="${id}"]`));
 
+
+const visibleSections = sectionIds.map(() => false);
+
 const options = {};
 const observer = new IntersectionObserver(observerCallback, options);
 sections.forEach(section => observer.observe(section));
 
 function observerCallback(entries) {
+    let selectLastOne;
     entries.forEach(entry => {
-        console.log(entry.target);
-        console.log(entry.isIntersecting);
-        console.log(entry.intersectionRatio);
-    })
+        const index = sectionIds.indexOf(`#${entry.target.id}`);
+        visibleSections[index] = entry.isIntersecting;
+        selectLastOne = 
+            index === sectionIds.length - 1 && 
+            entry.isIntersecting && 
+            entry.intersectionRatio >= 0.99;
+    });
+    console.log(visibleSections);
+    console.log('무조런 라스트 섹션', selectLastOne);
+
+    const navIndex = selectLastOne 
+    ? sectionIds.length - 1 
+    : findFirstIntersecting(visibleSections);
+    console.log(sectionIds(navIndex));
+}
+
+function findFirstIntersecting(intersections) {
+    const index = intersections.indexOf(true);
+    return index >= 0 ? index : 0;
 }
