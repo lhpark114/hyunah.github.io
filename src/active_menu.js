3c8044a2e222a1289f1c51f6821c07d9dd032d79
Chocolate covered Strawberry <75491899+lhpark114@users.noreply.github.com>
2023-07-12 18:05:40 -0400

    JS - active_menu_moving

--- src/active_menu.js
+++ src/active_menu.js
@@ -4,6 +4,7 @@ const navItems = sectionIds.map((id) => document.querySelector(`[href="${id}"]`)
 
 
 const visibleSections = sectionIds.map(() => false);
+let activeNavItem = navItems[0];
 
 const options = {
     rootMargin: '-20% 0px 0px 0px',
@@ -22,16 +23,25 @@ function observerCallback(entries) {
             entry.isIntersecting && 
             entry.intersectionRatio >= 0.99;
     });
-    console.log(visibleSections);
-    console.log('무조런 라스트 섹션', selectLastOne);
 
     const navIndex = selectLastOne 
     ? sectionIds.length - 1 
     : findFirstIntersecting(visibleSections);
-    console.log(sectionIds(navIndex));
+    selectNavItem(navIndex);
+
 }
+
 
 function findFirstIntersecting(intersections) {
     const index = intersections.indexOf(true);
     return index >= 0 ? index : 0;
+}
+
+function selectNavItem(index) {
+    const navItem = navItems[index];
+    if(!navItem) return;
+    activeNavItem.classList.remove('active');
+    activeNavItem = navItem;
+    activeNavItem.classList.add('active');
+
 }
